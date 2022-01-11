7867a2c6ebda1e40b10bdf67d720f8a4d48c1625
mrashutoshkrsingh <mrashutoshkrsingh@gmail.com>
2022-01-11 17:45:48 +0530

    mini app completed

---
+++ client/src/CommentList.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+export default function CommentList({ postId }) {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
+    setComments(res.data);
+  };
+
+  const renderedComments = comments.map((comment) => {
+    let content;
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+}
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 export default function PostList() {
   const [posts, setPosts] = useState({});
   const fetchPosts = async () => {
     try {
-      const res = await axios.get("http://localhost:4000/posts");
+      const res = await axios.get("http://localhost:4002/posts");
       setPosts(res.data);
     } catch (error) {
       alert(error);
@@ -16,6 +18,7 @@ export default function PostList() {
     fetchPosts();
   }, []);
 
+  // console.log(posts);
   const renderedPosts = Object.values(posts).map((post) => {
     return (
       <div
@@ -25,6 +28,8 @@ export default function PostList() {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
